6f806c806f57a5e8e09cb34562b85d7b7628588c
duchieuvn <tranthaiduchieu@gmail.com>
2022-08-12 10:09:56 +0700

    add Local Storage feature

--- App.js
+++ App.js
@@ -6,58 +6,61 @@ import {Provider} from 'react-redux';
 import Home from './src/screens/Home';
 import Details from './src/screens/Details';
 import Add from './src/screens/Add';
-import store from './src/redux/store';
+import {store, persistor} from './src/redux/store';
 
+import { PersistGate } from 'redux-persist/integration/react'
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
 
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name="Home" 
-            component={Home} 
-            options ={{
-              title: 'Home',
-              headerTitleAlign: "center",
-              headerStyle: {
-                backgroundColor: '#0288d1',
-              },
-              headerTitleStyle: {
-                color: "#FFF",
-                fontWeight: "bold",
-              }
-          }}/>
-          <Stack.Screen name="Details" 
-            component={Details} 
-            options ={{
-              title: 'Details',
-              headerTitleAlign: "center",
-              headerStyle: {
-                backgroundColor: '#0288d1',
-              },
-              headerTitleStyle: {
-                color: '#FFF',
-                fontWeight: "bold",
-              }
-          }}/>
-          <Stack.Screen name="Add" 
-            component={Add} 
-            options ={{
-              title: 'Add',
-              headerTitleAlign: "center",
-              headerStyle: {
-                backgroundColor: '#0288d1',
-              },
-              headerTitleStyle: {
-                color: '#FFF',
-                fontWeight: "bold",
-              }
-          }}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name="Home" 
+              component={Home} 
+              options ={{
+                title: 'Home',
+                headerTitleAlign: "center",
+                headerStyle: {
+                  backgroundColor: '#0288d1',
+                },
+                headerTitleStyle: {
+                  color: "#FFF",
+                  fontWeight: "bold",
+                }
+              }}/>
+            <Stack.Screen name="Details" 
+              component={Details} 
+              options ={{
+                title: 'Details',
+                headerTitleAlign: "center",
+                headerStyle: {
+                  backgroundColor: '#0288d1',
+                },
+                headerTitleStyle: {
+                  color: '#FFF',
+                  fontWeight: "bold",
+                }
+              }}/>
+            <Stack.Screen name="Add" 
+              component={Add} 
+              options ={{
+                title: 'Add',
+                headerTitleAlign: "center",
+                headerStyle: {
+                  backgroundColor: '#0288d1',
+                },
+                headerTitleStyle: {
+                  color: '#FFF',
+                  fontWeight: "bold",
+                }
+              }}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./reducer";
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
-const store = createStore(rootReducer)
-
-export default store
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
